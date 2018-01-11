59e09669a39845c33ffa229a513eea7d67d9db90
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-11 00:58:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 # Add  code here!
 def prime?(number)
-  (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
+  (2..number-1).each do |n|
+    return false if number <= 1 || number % n == 0
+  end
   return true
 end
